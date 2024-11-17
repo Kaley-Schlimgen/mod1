122a392c45eb9ba470450d171f3f313b8a3dcc63
Kaley-Schlimgen <kaleynicole89@gmail.com>
2024-11-17 12:18:15 -0700

    this is a test

---
+++ src/main/java/com/mycompany/p2mod1ct/P2Mod1CT.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.p2mod1ct;
+
+/**
+ *
+ * @author kaleyschlimgen
+ */
+public class P2Mod1CT {
+
+    public static void main(String[] args) {
+        Employee employeeItem = new Employee();
+        Manager managerItem = new Manager();
+        
+        employeeItem.setFirstName("Bob");
+        
+        employeeItem.printStatus();
+        
+        managerItem.setFirstName("Sue");
+        managerItem.setDepartment("Sales");
+        
+        managerItem.printStatus();
+        //System.out.println("Hello World!");
+        //this is a test
+    }
+}
